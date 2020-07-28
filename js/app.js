e5dd842a3c3615a4439a49d2171ec4c63c7fbc97
Hannah Kingsolver <58765774+hnkingsolver@users.noreply.github.com>
2020-07-28 16:18:34 -0500

    update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,43 @@
-const Hello = props => <h1>Hello {props.name}</h1>;
+//a Title component
+// const Title = props => (
+// <h1 className = "tc"> {props.title}</h1>
+// )
 
-ReactDOM.render(<Hello name="Hannah" />, document.getElementById('root'));
+//another Banner component
+// const Banner = props => (
+// <h1 className = "tc f1 yellow pa3">
+//     Hello, {props.firstName} {props.lastName}
+// </h1>
+// );
+// our components always need a single wrapper element at the top... like a div
+// const App = () => (
+//     <div>
+//         <Title title= "Welcome to my website!"/>
+//         <Banner firstName="Hannah" lastName="Kingsolver"/>
+//     </div>
+// )
+
+const Intro = () => 
+<div className= "m-auto-ns f4 f3-m f2-l tc w-80-l normal">
+    <div className="mb3 mb4-ns">
+        Lost in Tokyo is a directory of fun places to see, play in and explore, in Tokyo, Japan.
+    </div>
+    <div>
+    From museums and galleries, to robot restaurants and kitten cafes, Tokyo is the gift that
+    keeps on giving. Dattebayo!
+    </div>
+</div>
+
+
+const App = () => (
+    <div>
+        <div className="min-vh-100 ph4 flex flex-column">
+            {/* our navigation component */}
+            <Intro />
+            {/* our intro text component */}
+        </div>
+        <div className="flex flex-wrap container">{/* our attractions list component */}</div>
+    </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'));
