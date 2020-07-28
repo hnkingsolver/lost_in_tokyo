5af68b2d1d02c9b20c8db421639151926561ad61
Hannah Kingsolver <58765774+hnkingsolver@users.noreply.github.com>
2020-07-28 17:01:17 -0500

    highlights on intro

--- js/app.js
+++ js/app.js
@@ -17,14 +17,20 @@
 //     </div>
 // )
 
+const Highlight = props => (
+    <span className = {`relative highlight highlight-${props.color}`}>
+        <span className = "relative z-2">{props.children}</span>
+    </span>
+)
+
 const Intro = () => 
 <div className= "m-auto-ns f4 f3-m f2-l tc w-80-l normal">
     <div className="mb3 mb4-ns">
-        Lost in Tokyo is a directory of fun places to see, play in and explore, in Tokyo, Japan.
+        <Highlight color="aqua">Lost in Tokyo</Highlight> is a directory of fun places to see, play in and <Highlight color="yellow">explore</Highlight>, in <Highlight color="blue">Tokyo</Highlight>, Japan.
     </div>
     <div>
-    From museums and galleries, to robot restaurants and kitten cafes, Tokyo is the gift that
-    keeps on giving. Dattebayo!
+    From <Highlight color="blue">museums</Highlight> and <Highlight color="blue">galleries</Highlight>, to <Highlight color="pink">robot restaurants</Highlight> and <Highlight color="pink">kitten cafes</Highlight>, Tokyo is the gift that
+    keeps on giving. <Highlight color="yellow">Dattebayo!</Highlight>
     </div>
 </div>
 
